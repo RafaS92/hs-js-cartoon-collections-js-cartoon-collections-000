7e497b73330322e1ba4cabf6a28a4054039a34f3
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-02-10 19:55:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+
 function dwarfRollCall(dwarves) {
+  let newlist=[]
+for(let i = 0;i < dwarves.length;i++){
+  newlist.push (`${i+1}. ${dwarves[i]} `)
+}
+
+return newlist.join("")
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  let upperlist = []
+  for(let i = 0; i<planeteerCalls.length; i++){
+     upperlist.push(`${planeteerCalls[i]}`.toUpperCase() + '!')
+  }
+  return upperlist
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(calls){
+  for (let i = 0; i < calls.length; i++){
+    if (calls[i].length > 4) {
+      return true
+  }
+}
+return false
 }
 
-function findTheCheese (foods) {
+function findTheCheese(ingredients){
+  for (let i = 0; i < ingredients.length; i++){
+  if  ( ingredients[i] == "cheddar" || ingredients[i] == "gouda" || ingredients[i] == "camembert"){
+    return ingredients[i]
+  }
+}
+return "no cheese!"
 }
